2b9f74f2bba4fb35bbc2db98857341f3f3ed7fe3
Audrea Cook <audreacwork@gmail.com>
2019-07-15 22:00:49 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,17 @@
-# Add  code here!
+def prime?(num)
+  i = 1
+  if num <= 1
+    return false
+  end
+  
+  while i < num do
+    if (num % i == 0) && i != 1 && i != num && num 
+      return false
+    else
+      i += 1
+    end
+  end
+  
+  true
+end
+
