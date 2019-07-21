295da73ecd791b2bb98a6cc0049450effe6a7c38
Daniel Siekawitch <daniel.siekawitch@gmail.com>
2019-07-21 05:50:06 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,14 @@
-# Add  code here!
+def prime?(n)
+  start = 2
+  if n > 1
+    range = (start..n-1).to_a
+    
+# none of the numbers should return true for none? to return true. If none of the numbers
+# return true, then they are not divisible by the number and the number is prime 
+    range.none? do |num_to_test| 
+      n % num_to_test == 0
+    end
+  else
+    false
+  end
+end
