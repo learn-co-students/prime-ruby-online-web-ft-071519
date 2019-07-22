56e90b708474f1234d7673f2da2a66fb662afd36
Carl A Palumbo <carlpalumbo1@gmail.com>
2019-07-22 16:45:30 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,15 @@
-# Add  code here!
+def prime?(range)
+# array = * (range)
+  range.select do |optimus|
+    optimus.prime?()
+  end 
+end
+end
+=begin 
+def prime?(range)
+# array = * (range)
+  range.select do |optimus|
+    optimus.prime? 
+  end 
+end #Add  code here!
+=end 
