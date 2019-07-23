4f0060eaa10d6214fd583dd7b671f9d1423d3d8b
Winston  Chung <18.chung.winston@ctkprep.org>
2019-07-23 22:43:19 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,11 @@
-# Add  code here!
+def prime?(arg)
+  if arg == -1 
+    return false
+  elsif arg == 0
+    return false
+  elsif arg == 1
+    return false
+  else 
+  (2...arg).none? {|div| arg % div == 0}
+  end
+end
